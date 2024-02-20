a95f27ca215627d7471390d1dc995638abecec88
Pejoska99 <biljanapejoska985@gmail.com>
2024-02-20 18:53:07 +0100

    new commit 1

---
+++ validation.js
@@ -0,0 +1,9 @@
+function validateInput(input) {
+    console.log(input);
+    if (input.value.length < 3 || input.value.length > 15) {
+        input.classList.add("is-invalid");
+    }else {
+        input.classList.remove ("is-invalid");
+        input.classList.add ("is-valid");
+    }
+}
